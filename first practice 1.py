b7308010bcfc0118aa98e49068b5bf532cdc41e4
zulkharnayeva17 <96519587+zulkharnayeva17@users.noreply.github.com>
2022-09-08 22:29:03 +0500

    Add files via upload

---
+++ first practice 1.py
@@ -0,0 +1,25 @@
+Practice 1:
+    
+1)
+
+x=7
+y=8
+print ('x = ',x)
+print ('y = ',y)
+z=x+y
+print ('z = ',z)
+z=x-y
+print ('z =',z )
+z=x*y
+print ('z =',z )
+z=x/y 
+print ('z =',z )
+
+
+
+2)  a= input ('Your last name, first name')
+b= input (" How old are you? ")
+c= input (" Where do you live?")
+print ('Your name', a)
+print ('Your age', b)
+print (' Your live in',c)
